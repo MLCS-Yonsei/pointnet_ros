c6aadd419194664a30c19d9f2a5ff94b19e8a060
Jaehyun Lim <jaehyunlim@yonsei.ac.kr>
2020-11-18 21:48:16 +0900

    Add PoseArray publisher

--- scripts/pointnet_ros.py
+++ scripts/pointnet_ros.py
@@ -2,8 +2,13 @@
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import Pose
 from rhd3d import HumanDetector3D
 import numpy as np
+
+import pcl
+import pcl_helper
 
 # subscribe lidar msgs and change to numpy.ndarray
 # publish centroid by pose array msgs from pointnet func 
@@ -14,20 +19,45 @@ import numpy as np
 # quaterion x,y,z,w 2 -> object id
 
 
-#pointcloud data to ndarray 
-def callback(data):
-    detector = HumanDetector3D(normalize_intensity=True)
-    N = data.width #changed to pointcloud width
+class PointnetROS:
 
-    #points = N x (x, y, z, intensity)
-    points = np.zeros((N, 4)).astype(np.float32)
-    results = detector.get_result(points)
-    
-    
-def listener():
-    rospy.init_node('pointcloud_listener', anonymous=True)
-    rospy.Subscriber("/scan_matched_points2", PointCloud2, callback)
-    rospy.spin()
+    def __init__(self):
+
+        rospy.init_node('pointcloud_listener', anonymous=True)
+
+        posearray_topic = rospy.get_param("posearray_topic")
+        self.pub = rospy.Publisher(posearray_topic, PoseArray, queue_size=10)
+
+        rospy.Subscriber("/scan_matched_points2", PointCloud2, self.callback)
+
+        self.detector = HumanDetector3D(normalize_intensity=True)
+
+        rospy.spin()
+
+
+    def callback(self, data):
+        #pointcloud data to ndarray 
+        N = data.width #changed to pointcloud width
+
+        #points = N x (x, y, z, intensity)
+        points = pcl_helper.ros_to_pcl(data)
+
+        results = self.detector.get_result(points)
+
+        msg = PoseArray()
+        msg.header = rospy.Time.now()
+        for object_id in range(results.shape[0]):
+            pose = Pose()
+            pose.orientation.w = object_id
+            pose.orientation.x = results[object_id, 0]
+            pose.orientation.y = results[object_id, 1]
+            pose.orientation.z = results[object_id, 2]
+            msg.poses.append(pose)
+
+        self.pub.publish(msg)
+
+
 
 if __name__ == '__main__':
-    listener()
+
+    pointnet = PointnetROS()
